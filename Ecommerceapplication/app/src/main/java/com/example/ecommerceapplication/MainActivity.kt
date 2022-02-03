72908bbb8afd5b9650ffab15fbabd7447652dee7
Wassim Boukries <wassim-boukries@live.fr>
2022-02-04 00:58:37 +0100

    [FEAT] Adding the search Bar allowing the search of specific products

--- Ecommerceapplication/app/src/main/java/com/example/ecommerceapplication/MainActivity.kt
+++ Ecommerceapplication/app/src/main/java/com/example/ecommerceapplication/MainActivity.kt
@@ -4,13 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         val policy = ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+
+        val searchItem = menu?.findItem(R.id.nav_search)
+        val searchView = searchItem?.actionView as SearchView
+
+        searchView.queryHint = "Search a product"
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String): Boolean {
+                Log.v(TAG, p0)
+                return true
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
